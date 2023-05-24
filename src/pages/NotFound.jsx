6deebfa0ef6b9b14e5dcf3b24f57227664762b72
Makz Sánchez <makzofx@gmail.com>
2023-05-24 01:18:05 -0300

    agregado un Link para volver al home desde la 404

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,10 +1,15 @@
-import { useRouteError } from "react-router-dom";
+import { useRouteError, Link } from "react-router-dom";
 
 const NotFound = () => {
   const error = useRouteError();
   console.log(error);
 
-  return <h1>{error.data && error.statusText}</h1>;
+  return (
+    <>
+      <h1>{error.statusText || error.data}</h1>;
+      <Link className="btn btn-danger" to="/">Volver al home </Link>
+    </>
+  );
 };
 
 export default NotFound;
