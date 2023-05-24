faf6651d058886e4f93b834ee04316a04b626a4d
Makz Sánchez <makzofx@gmail.com>
2023-05-24 01:11:03 -0300

    not found funcional

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,7 +3,8 @@ import { useRouteError } from "react-router-dom";
 const NotFound = () => {
   const error = useRouteError();
   console.log(error);
-  return "notfound";
+
+  return <h1>{error.data && error.statusText}</h1>;
 };
 
 export default NotFound;
