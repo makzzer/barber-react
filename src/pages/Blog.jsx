0b68189db779e9c2e75d2e70dd85f4e6c734263d
Makz Sánchez <makzofx@gmail.com>
2023-05-28 19:15:55 -0300

    agregado ternario en consumo api+Link en elemlist

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,25 +1,29 @@
 //Acá voy a usar el HOOK UseLoaderData para traerme lo que devuelva la llamada de los post del blog y lo voy a usar para determinar que ese resultado va a ser el array que consumo
 
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 
 const Blog = () => {
-
-
-    {/*Entre comillas le tengo que pasar si o si el mismo nombre del objeto que exporté en la funcion ASYNC de consulta a la API*/}
-    const { datosTraidos } = useLoaderData();
-    console.log(datosTraidos)
+  {
+    /*Entre comillas le tengo que pasar si o si el mismo nombre del objeto que exporté en la funcion ASYNC de consulta a la API*/
+  }
+  const { datosTraidos } = useLoaderData();
+  console.log(datosTraidos);
 
   return (
     <>
-      <h1>Blog</h1>;
+      <h1>Blog</h1>
       <div>
-        <ul className="list-group">{
-            
-            datosTraidos.map ( datos => (
-                <li key={datos.id} className="list-group-item">{datos.id} {datos.title} </li>
+        <ul className="list-group">
+          {datosTraidos.length > 0 
+          ? (datosTraidos.map((dato) => (
+              <li key={dato.id} className="list-group-item">
+                <Link to={`/blog/${dato.id}`}>
+                  {dato.id} {dato.title}
+                </Link>
+              </li>
             ))
-            
-            }</ul>
+          ) : (<li>datos no encontrados</li>)}
+        </ul>
       </div>
     </>
   );
