d0fc90d5af4caaa8506b800d46037f1dffa2eda9
Maximiliano <makzofx@gmail.com>
2023-05-30 20:41:15 -0300

    manejo de errores en post y blog

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -10,24 +10,22 @@ const Blog = () => {
   }
   const { datosTraidos } = useLoaderData();
 
-
   return (
     <>
       <h1>Blog</h1>
       <div>
         <ul className="list-group">
-          {datosTraidos.length > 0 
-
-          ? (datosTraidos.map((dato) => (
+          {datosTraidos.length > 0 ? (
+            datosTraidos.map((dato) => (
               <li key={dato.id} className="list-group-item">
                 <Link to={`/blog/${dato.id}`}>
                   {dato.id} {dato.title}
                 </Link>
               </li>
             ))
-          ) 
-          
-          : (<li>datos no encontrados</li>)}
+          ) : (
+            <li>datos no encontrados</li>
+          )}
         </ul>
       </div>
     </>
@@ -38,7 +36,22 @@ export default Blog;
 
 //voy a crear una funcion asincrona para consumir desde la api de datos
 export const loaderBlog = async () => {
-  const llamada = await fetch("https://jsonplaceholder.typicode.com/posts");
-  const datosTraidos = await llamada.json();
+  const respuesta = await fetch("https://jsonplaceholder.typicode.com/posts");
+  console.log(respuesta.status)
+
+  //para capturar el error en caso de que la solicitud falle
+  //en las promises el catch lo tira react automaticamente, solo hago el twrow
+  //tengo que usar el respuesta.status===404 porque el respuesta.ok probablemente me esté devolviendo true
+  if (respuesta.status==404)
+    throw {
+      //data: "No se pudo acceder al elemento de la API",
+      statusText: "Codigo del error: " + respuesta.status,
+    };
+
+
+  //si no falla, que me la pase a json y me la devuelva
+  const datosTraidos = await respuesta.json();
+  console.log(respuesta);
+
   return { datosTraidos };
 };
--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -6,8 +6,9 @@ const Post = () => {
 
   return (
     <>
+      {}
+
       <h1>
-        
         {postRecibido.id} {postRecibido.title}{" "}
       </h1>
       <p>{postRecibido.body}</p>
@@ -24,6 +25,14 @@ export const loaderPosts = async ({ params }) => {
   const data = await fetch(
     `https://jsonplaceholder.typicode.com/posts/${params.id}`
   );
+
+  //en las promises el catch lo tira react automaticamente, solo hago el twrow
+  if (!data.ok)
+    throw {
+      data: "Post no encontrado",
+      //statusText: "Codigo del error: " + data.status,
+    };
+
   const postRecibido = await data.json();
   return { postRecibido };
 };
