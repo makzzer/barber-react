b07aac0f105c0c8a4fc279a5804841db264e8468
Maximiliano <makzofx@gmail.com>
2023-05-30 18:45:46 -0300

    agregado loading con hook useNavigation

--- src/layouts/LayoutPublic.jsx
+++ src/layouts/LayoutPublic.jsx
@@ -1,8 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { Outlet,useNavigation } from "react-router-dom";
 import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
 
 const LayoutPublic = () => {
+
+
+  //esto es para cargar el Loading...
+  const navegador = useNavigation()
+  console.log(navegador.state)
+
+
   {
     /* uso el <Outlet/> para linkear con el contenido dinamico de las distintas
     paginas, esto es lo que está dentro de :children en el router/index.jsx */
@@ -11,7 +18,16 @@ const LayoutPublic = () => {
     <>
      <Navbar/>
       <main className="text-center container">
-        <Outlet />
+        {/*acá voy a agregar el loading para cuando esté cargando */}
+
+        {
+          (navegador.state==="loading") 
+          ? <h1>Cargandooo</h1>
+          :  <Outlet />
+        }
+
+
+        
       </main>
       <Footer/>
       </>
