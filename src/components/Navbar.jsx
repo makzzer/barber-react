8890145a0cc0134e5ecbe531ba6dad6749fe1d89
Maximiliano <makzofx@gmail.com>
2024-01-05 02:59:31 -0300

    mejorado nav con home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,15 @@ const Navbar = () => {
   return (
     <>
       <div className="container-fluid d-flex navbar-dark navbar bg-dark p-4 ">
-        <NavLink className="btn btn-outline-danger text-light" to="/turnos"> Turnos </NavLink>
-        <NavLink className="btn btn-outline-danger text-light" to="/ingresar"> Ingresar </NavLink>
+        <NavLink className="btn btn-outline-danger text-light" to="/">
+          {" "}
+          Home{" "}
+        </NavLink>
+        {/** <NavLink className="btn btn-outline-danger text-light" to="/turnos"> Turnos </NavLink>*/}
+        <NavLink className="btn btn-outline-danger text-light" to="/ingresar">
+          {" "}
+          Ingresar{" "}
+        </NavLink>
       </div>
     </>
   );
