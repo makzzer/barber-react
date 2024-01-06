2ffb484f3a52afdc8783bf5aefcc648d6ec68cee
Maximiliano <makzofx@gmail.com>
2024-01-05 21:27:53 -0300

    en construc

--- src/pages/Ingresar.jsx
+++ src/pages/Ingresar.jsx
@@ -1,8 +1,7 @@
 const Ingresar = () => {
   return (
     <>
-      <div className="container text-center mt-4 mb-4 bg-body-tertiary">
-        <h1 className="mb-8">INGRESAR</h1>
+      <div className="p-4 container text-center mt-4 mb-4 bg-body-tertiary">
         <h1 className="mb-8 mt-4">SITIO EN CONSTRUCCION</h1>
       </div>{" "}
     </>
