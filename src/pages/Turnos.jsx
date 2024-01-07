a381042910938abb790a904f49b8c3ae8b4de644
Maximiliano <makzofx@gmail.com>
2024-01-07 19:39:08 -0300

    mejoras esteticas boton volver

--- src/pages/Turnos.jsx
+++ src/pages/Turnos.jsx
@@ -1,11 +1,24 @@
 
 import ListaTurnos from "../components/ListaTurnos"
+import { NavLink } from "react-router-dom";
 
 const Turnos = () => {
   return (
     <div className="container text-center mt-4 mb-4">
       <h1 className="mb-12">MIS TURNOS</h1>
       <ListaTurnos />
+
+      <div className="row d-flex gap-2 custom-container-boton mt-2">
+          <div className="d-flex flex-column m-2 uppercase">
+
+
+            <NavLink className="btn btn-danger  m-2 p-4 pb-10" to="/">
+              Volver
+            </NavLink>
+          </div>
+        </div>
+      
+      
     </div>
   );
 };
